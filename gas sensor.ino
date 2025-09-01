5fe0332c90b2237fcd6f8eed0c9471c24d91e822
Keshava-p <keshava.22cs079@sode-edu.in>
2025-09-01 15:56:02 +0530

    Add files via upload

---
+++ gas sensor.ino
@@ -0,0 +1,27 @@
+int sensor=0;
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(5,OUTPUT);
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  sensor=analogRead(A0);
+  Serial.println(sensor);
+  if(sensor>=90){
+    digitalWrite(5,HIGH);
+  }
+  else{
+     digitalWrite(5,LOW);
+  }
+  if(sensor>=110){
+    digitalWrite(7,HIGH);
+  }
+  else{
+     digitalWrite(7,LOW);
+  }
+  delay(100);
+}
